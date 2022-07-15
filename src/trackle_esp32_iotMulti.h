e12fbfbf157fd19379890cced1a0771fb2208bb9
flaz83 <flavio.ferrandi@gmail.com>
2022-07-15 15:46:38 +0200

    fix: update WiFiEvents

--- src/trackle_esp32_iotMulti.h
+++ src/trackle_esp32_iotMulti.h
@@ -18,7 +18,7 @@
 
 static bool eth_connected = false;
 static bool wifi_connected = false;
-uint8_t network_type = 0; //0 ethernet; 1 wifi
+uint8_t network_type = 0; // 0 ethernet; 1 wifi
 WiFiUDP udp;
 wifi_event_id_t wifi_event_id = 0;
 
@@ -29,48 +29,48 @@ void WiFiEvent(WiFiEvent_t event)
 {
   switch (event)
   {
-  case SYSTEM_EVENT_ETH_START:
-    log_callback("Ethernet Started", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_ETH_START:
+    log_callback("Ethernet Started", INFO, "Trackle", NULL, NULL);
     ETH.setHostname("esp32-ethernet");
     break;
-  case SYSTEM_EVENT_ETH_CONNECTED:
-    log_callback("Ethernet Connected", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_ETH_CONNECTED:
+    log_callback("Ethernet Connected", INFO, "Trackle", NULL, NULL);
     break;
-  case SYSTEM_EVENT_ETH_GOT_IP:
-    log_callback("Ethernet MAC", INFO, "Trackle" , NULL, NULL);
-    log_callback(ETH.macAddress().c_str(), INFO, "Trackle" , NULL, NULL);
-    log_callback("Ethernet IPv4", INFO, "Trackle" , NULL, NULL);
-    log_callback(ETH.localIP().toString().c_str(), INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_ETH_GOT_IP:
+    log_callback("Ethernet MAC", INFO, "Trackle", NULL, NULL);
+    log_callback(ETH.macAddress().c_str(), INFO, "Trackle", NULL, NULL);
+    log_callback("Ethernet IPv4", INFO, "Trackle", NULL, NULL);
+    log_callback(ETH.localIP().toString().c_str(), INFO, "Trackle", NULL, NULL);
     eth_connected = true;
     break;
-  case SYSTEM_EVENT_ETH_DISCONNECTED:
-    log_callback("Ethernet Disconnected", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_ETH_DISCONNECTED:
+    log_callback("Ethernet Disconnected", INFO, "Trackle", NULL, NULL);
     eth_connected = false;
     break;
-  case SYSTEM_EVENT_ETH_STOP:
-    log_callback("Ethernet Stopped", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_ETH_STOP:
+    log_callback("Ethernet Stopped", INFO, "Trackle", NULL, NULL);
     eth_connected = false;
     break;
-  case SYSTEM_EVENT_STA_START:
-    log_callback("WIFI Started", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_WIFI_STA_START:
+    log_callback("WIFI Started", INFO, "Trackle", NULL, NULL);
     WiFi.setHostname("esp32-WiFi");
     break;
-  case SYSTEM_EVENT_STA_CONNECTED:
-    log_callback("WIFI Connected", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_WIFI_STA_CONNECTED:
+    log_callback("WIFI Connected", INFO, "Trackle", NULL, NULL);
     break;
-  case SYSTEM_EVENT_STA_GOT_IP:
-    log_callback("WiFi MAC", INFO, "Trackle" , NULL, NULL);
-    log_callback(WiFi.macAddress().c_str(), INFO, "Trackle" , NULL, NULL);
-    log_callback("WiFi IPv4", INFO, "Trackle" , NULL, NULL);
-    log_callback(WiFi.localIP().toString().c_str(), INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+    log_callback("WiFi MAC", INFO, "Trackle", NULL, NULL);
+    log_callback(WiFi.macAddress().c_str(), INFO, "Trackle", NULL, NULL);
+    log_callback("WiFi IPv4", INFO, "Trackle", NULL, NULL);
+    log_callback(WiFi.localIP().toString().c_str(), INFO, "Trackle", NULL, NULL);
     wifi_connected = true;
     break;
-  case SYSTEM_EVENT_STA_DISCONNECTED:
-    log_callback("WiFi Disconnected", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+    log_callback("WiFi Disconnected", INFO, "Trackle", NULL, NULL);
     wifi_connected = false;
     break;
-  case SYSTEM_EVENT_STA_STOP:
-    log_callback("WiFi Stopped", INFO, "Trackle" , NULL, NULL);
+  case ARDUINO_EVENT_WIFI_STA_STOP:
+    log_callback("WiFi Stopped", INFO, "Trackle", NULL, NULL);
     wifi_connected = false;
     break;
   default:
@@ -106,7 +106,7 @@ void log_callback(const char *msg, int level, const char *category, void *attrib
 int connect_cb_udp(const char *address, int port)
 {
   int ret = -1;
-  //printf("connect_cb_udp\n");
+  // printf("connect_cb_udp\n");
   server_port = port;
   udp.begin(server_port);
   udp.setTimeout(100);
@@ -114,7 +114,7 @@ int connect_cb_udp(const char *address, int port)
   if (eth_connected || wifi_connected)
   {
     ret = WiFi.hostByName(address, server_ip);
-    //printf("ret: %d, %s %d.%d.%d.%d\n", ret, address, server_ip[0], server_ip[1], server_ip[2], server_ip[3]);
+    // printf("ret: %d, %s %d.%d.%d.%d\n", ret, address, server_ip[0], server_ip[1], server_ip[2], server_ip[3]);
     if (ret == 1)
       Trackle.connectionCompleted();
   }
@@ -140,8 +140,8 @@ int send_cb_udp(const unsigned char *buf, uint32_t buflen, void *tmp)
 
   if (sent > 0)
   {
-    //printf("sent ");
-    //printfln(sent);
+    // printf("sent ");
+    // printfln(sent);
   }
   return sent;
 }
@@ -155,8 +155,8 @@ int receive_cb_udp(unsigned char *buf, uint32_t buflen, void *tmp)
   }
   if (rcv > 0)
   {
-    //printf("rcv ");
-    //printfln(rcv);
+    // printf("rcv ");
+    // printfln(rcv);
   }
   return rcv;
 }
@@ -168,7 +168,8 @@ int my_configure_ip(IPAddress ip, IPAddress gateway, IPAddress subnet, IPAddress
   return 1;
 }
 
-void configWifiEvents() {
+void configWifiEvents()
+{
   if (wifi_event_id <= 0)
     wifi_event_id = WiFi.onEvent(WiFiEvent);
 }
@@ -191,4 +192,4 @@ void my_loop()
 {
 }
 
-#endif //TrackleEthWifi_h
+#endif // TrackleEthWifi_h
